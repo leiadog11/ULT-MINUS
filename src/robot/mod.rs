696bc22657a332a4bb368f829894110e5716d55e
Daniel Palomarez <daniel.palomarez5@gmail.com>
2024-05-06 18:24:04 -0600

    top buries (idk how to make it random) added hit detect code to robot folder

--- src/robot/mod.rs
+++ src/robot/mod.rs
@@ -10,6 +10,13 @@ use {
     smashline::*
 };
 
+use skyline::nn::ro::LookupSymbol;
+use skyline::hooks::{Region,getRegionAddress};
+use skyline::libc::*;
+
+static mut NOTIFY_LOG_EVENT_COLLISION_HIT_OFFSET : usize = 0x675A20;
+const FIGHTER_ROBOT_INSTANCE_WORK_ID_FLAG_SEARCH_HIT : i32 = 0x200000eb;
+
 // ------GROUND------
 
 // UP TILT
@@ -498,8 +505,51 @@ unsafe extern "C" fn rob_frame(fighter: &mut L2CFighterCommon) {
     }
 }
 
+// #[skyline::hook(offset = NOTIFY_LOG_EVENT_COLLISION_HIT_OFFSET)]
+// pub unsafe fn notify_log_event_collision_hit_replace(fighter_manager: *mut smash::app::FighterManager, attacker_id: u32, defender_id: u32, move_type: f32, arg5: i32, move_type_again: bool, fighter: &mut L2CAgentBase) -> u64 {
+//     let attacker_boma = sv_battle_object::module_accessor(attacker_id);
+//     let defender_boma = sv_battle_object::module_accessor(defender_id);
+//     let attacker_kind = sv_battle_object::kind(attacker_id);
+//     let defender_kind = sv_battle_object::kind(defender_id);
+//     // if search_hit flag is on
+//     if WorkModule::is_flag(attacker_boma, FIGHTER_ROBOT_INSTANCE_WORK_ID_FLAG_SEARCH_HIT) {
+//         // disable flag
+//         WorkModule::off_flag(attacker_boma, FIGHTER_ROBOT_INSTANCE_WORK_ID_FLAG_SEARCH_HIT);
+//         // if thing being hit is a fighter
+//         if utility::get_category(&mut *defender_boma) == *BATTLE_OBJECT_CATEGORY_FIGHTER {
+//             // give fighter being hit gyro
+//             ItemModule::have_item(attacker_boma, smash::app::ItemKind(*ITEM_KIND_ROBOTGYRO), 0, 0, false, false);
+//         }
+//     }
+
+
+fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    haystack.windows(needle.len()).position(|window| window == needle)
+}
+
+static OFFSET_SEARCH_CODE: &[u8] = &[
+    0xff, 0x03, 0x03, 0xd1, //.text:0000007100675A20                 SUB             SP, SP, #0xC0
+    0xe8, 0x2b, 0x00, 0xfd, //.text:0000007100675A24                 STR             D8, [SP,#0xB0+var_60]
+    0xfc, 0x6f, 0x06, 0xa9, //.text:0000007100675A28                 STP             X28, X27, [SP,#0xB0+var_50]
+    0xfa, 0x67, 0x07, 0xa9, //.text:0000007100675A2C                 STP             X26, X25, [SP,#0xB0+var_40]
+    0xf8, 0x5f, 0x08, 0xa9, //.text:0000007100675A30                 STP             X24, X23, [SP,#0xB0+var_30]
+    0xf6, 0x57, 0x09, 0xa9, //.text:0000007100675A34                 STP             X22, X21, [SP,#0xB0+var_20]
+    0xf4, 0x4f, 0x0a, 0xa9, //.text:0000007100675A38                 STP             X20, X19, [SP,#0xB0+var_10]
+    0xfd, 0x7b, 0x0b, 0xa9, //.text:0000007100675A3C                 STP             X29, X30, [SP,#0xB0+var_s0]
+    0xfd, 0xc3, 0x02, 0x91, //.text:0000007100675A40                 ADD             X29, SP, #0xB0
+    0xfb, 0x03, 0x00, 0xaa  //.text:0000007100675A44                 MOV             X27, X0
+];
+
 
 pub fn install() {
+    unsafe {
+        let text_ptr = getRegionAddress(Region::Text) as *const u8;
+        let text_size = (getRegionAddress(Region::Rodata) as usize) - (text_ptr as usize);
+        let text = std::slice::from_raw_parts(text_ptr, text_size);
+        if let Some(offset) = find_subsequence(text, OFFSET_SEARCH_CODE) {
+            NOTIFY_LOG_EVENT_COLLISION_HIT_OFFSET = offset;
+        }
+    }
     Agent::new("robot")
     .game_acmd("game_attackhi3", rob_attackhi3)
     .game_acmd("game_attacks3hi", rob_attacks3hi)
