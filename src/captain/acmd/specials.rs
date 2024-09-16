75f5fa8c34dcc2b54c9876f604ccaa017ff241b5
Jmuze <jmuze99@gmail.com>
2024-09-15 21:52:26 -0600

    falcon is so close.... (and i opened the folder for game n watch. havent done anything on it yet)

--- src/captain/acmd/specials.rs
+++ src/captain/acmd/specials.rs
@@ -84,6 +84,60 @@ unsafe extern "C" fn captain_specialairsstart(agent: &mut L2CAgentBase) {
     }
 }
 
+//UP SPECIAL
+unsafe extern "C" fn captain_specialhi(agent: &mut L2CAgentBase) {
+    frame(agent.lua_state_agent, 2.0);
+    if macros::is_excute(agent) {
+        macros::ATTACK(agent, 0, 0, Hash40::new("rot"), 5.0, 361, 100, 0, 50, 9.6, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_fire"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_NONE);
+        AttackModule::set_catch_only_all(agent.module_accessor, true, false);
+    }
+    wait(agent.lua_state_agent, 13.0);
+    if macros::is_excute(agent) {
+        AttackModule::clear_all(agent.module_accessor);
+    }
+}
+
+//UP SPECIAL EFFECT
+unsafe extern "C" fn captain_effect_specialhi(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        macros::EFFECT(agent, Hash40::new("sys_crown"), Hash40::new("top"), 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+        macros::EFFECT(agent, Hash40::new("sys_bomb_a"), Hash40::new("top"), 7.5, 12, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
+    }
+    frame(agent.lua_state_agent, 2.0);
+    if macros::is_excute(agent) {
+        macros::EFFECT_FOLLOW(agent, Hash40::new("sys_damage_fire"), Hash40::new("havel"), 0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(agent.lua_state_agent, 12.0);
+    if macros::is_excute(agent) {
+        macros::EFFECT_FOLLOW(agent, Hash40::new("sys_damage_fire"), Hash40::new("havel"), 0, 0, 0, 0, 0, 0, 1, true);
+    }
+    frame(agent.lua_state_agent, 22.0);
+    if macros::is_excute(agent) {
+        macros::EFFECT_FOLLOW(agent, Hash40::new("sys_damage_fire"), Hash40::new("havel"), 0, 0, 0, 0, 0, 0, 1, true);
+    }
+}
+
+//UP SPECIAL SOUND
+unsafe extern "C" fn captain_sound_specialhi(agent: &mut L2CAgentBase) {
+    frame(agent.lua_state_agent, 1.0);
+    if macros::is_excute(agent) {
+        macros::PLAY_SE(agent, Hash40::new("se_captain_special_h03"));
+        macros::PLAY_SE(agent, Hash40::new("vc_captain_appeal03"));
+    }
+    wait(agent.lua_state_agent, 40.0);
+    if macros::is_excute(agent) {
+        macros::PLAY_SE(agent, Hash40::new("se_captain_special_s01"));
+    }
+}
+
+//UP SPECIAL EXPRESSION
+unsafe extern "C" fn captain_expression_specialhi(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        macros::QUAKE(agent, *CAMERA_QUAKE_KIND_M);
+        ControlModule::set_rumble(agent.module_accessor, Hash40::new("rbkind_explosionm"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
+    }
+}
+
 
 pub fn install() {
     Agent::new("captain")
@@ -95,6 +149,11 @@ pub fn install() {
         .game_acmd("game_specialsstart", captain_specialsstart, Low)
 
         .game_acmd("game_specialairsstart", captain_specialairsstart, Low)
+
+        .game_acmd("game_specialhi", captain_specialhi, Low)
+        .effect_acmd("effect_specialhithrow", captain_effect_specialhi, Low)
+        .sound_acmd("sound_specialhithrow", captain_sound_specialhi, Low)
+        .expression_acmd("expression_specialhithrow", captain_expression_specialhi, Low)
 
         .install();
 }
