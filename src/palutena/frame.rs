e00536b9122c9ee2503b037f2c22c84192726738
leiadog11 <jcrho3005@gmail.com>
2026-06-02 18:24:24 -0600

    Palutena Nerfs + Polish

--- src/palutena/frame.rs
+++ src/palutena/frame.rs
@@ -10,6 +10,19 @@ pub unsafe extern "C" fn palutena_frame(fighter: &mut L2CFighterCommon) {
         let situation_kind = StatusModule::situation_kind(boma);
         let frame = MotionModule::frame(boma);
 
+        // ON RESPAWN
+        if status_kind == *FIGHTER_STATUS_KIND_REBIRTH { 
+            if MEGA_LASER_CHARGE[ENTRY_ID] >= 360 { 
+                EffectModule::req_follow(fighter.module_accessor, Hash40::new("sys_mball_flash"), Hash40::new("stick"), &vector, &vector, 1.0, true, 0, 0, 0, 0, 0, true, true) as u32;
+                EffectModule::enable_sync_init_pos_last(fighter.module_accessor);
+            }
+            if BLACKHOLE_CHARGE[ENTRY_ID] >= 360 { 
+                let effect = EffectModule::req_follow(fighter.module_accessor, Hash40::new("palutena_wand_light_trace"), Hash40::new("stick"), &dumb, &dumb, 1.0, true, 0, 0, 0, 0, 0, true, true) as u32;
+                EffectModule::set_rgb(fighter.module_accessor, effect, 0.5, 0.0, 0.5);
+                EffectModule::enable_sync_init_pos_last(fighter.module_accessor);
+            }
+        }
+
         // ON HIT
         if DamageModule::reaction(boma, 0) > 1.0 {
             ArticleModule::remove_exist(boma, *FIGHTER_PALUTENA_GENERATE_ARTICLE_GODWING, ArticleOperationTarget(*ARTICLE_OPE_TARGET_ALL)); // CLEAR WINGS
--- src/palutena/status/specialn.rs
+++ src/palutena/status/specialn.rs
@@ -172,7 +172,7 @@ unsafe extern "C" fn palutena_specialn_charge_main_loop(fighter: &mut L2CFighter
         let vector = Vector3f{x:0.0,y:5.0,z:0.0};
         EffectModule::req_on_joint(fighter.module_accessor, Hash40::new("sys_flash"), Hash40::new("hip"), &vector, &vector, 1.2, &vector, &vector, false, 0, 0, 0);
         SoundModule::play_se(fighter.module_accessor, Hash40::new("se_gohoubi_icon_money"), true, false, false, false, enSEType(0));
-        let effect = EffectModule::req_follow(fighter.module_accessor, Hash40::new("sys_mball_flash"), Hash40::new("stick"), &vector, &vector, 1.0, true, 0, 0, 0, 0, 0, true, true) as u32;
+        EffectModule::req_follow(fighter.module_accessor, Hash40::new("sys_mball_flash"), Hash40::new("stick"), &vector, &vector, 1.0, true, 0, 0, 0, 0, 0, true, true) as u32;
         EffectModule::enable_sync_init_pos_last(fighter.module_accessor);
         if StatusModule::situation_kind(fighter.module_accessor) != *SITUATION_KIND_AIR { 
             fighter.change_status(FIGHTER_STATUS_KIND_WAIT.into(), false.into());
