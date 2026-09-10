6f7df4a736b29d2ef71f64c317d9db9c42d0ef21
leiadog11 <jcrho3005@gmail.com>
2026-09-10 16:11:03 -0600

    Ridley Down Throw Fix

--- src/ridley/acmd/throws.rs
+++ src/ridley/acmd/throws.rs
@@ -69,113 +69,6 @@ unsafe extern "C" fn ridley_catchturn(agent: &mut L2CAgentBase) {
     }
 }
 
-/*
-
-// DOWN THROW
-unsafe extern "C" fn ridley_throwlw(agent: &mut L2CAgentBase) {
-    frame(agent.lua_state_agent, 18.0);
-    if macros::is_excute(agent) {
-        macros::ATTACK(agent, 0, 0, Hash40::new("handl"), 6.0, 0, 0, 0, 0, 8.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_PUNCH);
-    }
-    frame(agent.lua_state_agent, 20.0);
-    if macros::is_excute(agent) { 
-        AttackModule::clear_all(agent.module_accessor);
-    }
-    frame(agent.lua_state_agent, 38.0);
-    if macros::is_excute(agent) {
-        macros::ATTACK(agent, 1, 0, Hash40::new("handl"), 6.0, 0, 0, 0, 0, 8.0, 0.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_NONE, *ATTACK_REGION_THROW);
-    }
-    frame(agent.lua_state_agent, 40.0);
-    if macros::is_excute(agent) { 
-        AttackModule::clear_all(agent.module_accessor);
-        GrabModule::set_rebound(agent.module_accessor, true);
-    }
-    frame(agent.lua_state_agent, 41.0);
-    if macros::is_excute(agent) { 
-        macros::CATCH(agent, 0, Hash40::new("handl"), 15.0, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), *FIGHTER_STATUS_KIND_CAPTURE_PULLED, *COLLISION_SITUATION_MASK_GA);
-        macros::CATCH(agent, 1, Hash40::new("handl"), 12.5, 0.0, 0.0, 0.0, Some(0.0), Some(0.0), Some(0.0), *FIGHTER_STATUS_KIND_CAPTURE_PULLED, *COLLISION_SITUATION_MASK_GA);
-    }
-    macros::game_CaptureCutCommon(agent);
-    wait(agent.lua_state_agent, 3.0);
-    if macros::is_excute(agent) {
-        grab!(agent, *MA_MSC_CMD_GRAB_CLEAR_ALL);
-        WorkModule::on_flag(agent.module_accessor, *FIGHTER_STATUS_CATCH_FLAG_CATCH_WAIT);
-        GrabModule::set_rebound(agent.module_accessor, false);
-    }
-}
-
-// DOWN THROW EFFECT
-unsafe extern "C" fn ridley_effect_throwlw(agent: &mut L2CAgentBase) {
-    frame(agent.lua_state_agent, 11.0);
-    if macros::is_excute(agent) {
-        macros::EFFECT(agent, Hash40::new("sys_smash_flash_s"), Hash40::new("throw"), -2, 10, 5, 0, 0, 0, 1.5, 0, 0, 0, 0, 0, 0, true);
-    }
-    frame(agent.lua_state_agent, 17.0);
-    if macros::is_excute(agent) {
-        macros::EFFECT_FOLLOW_FLIP(agent, Hash40::new("sys_attack_speedline"), Hash40::new("sys_attack_speedline"), Hash40::new("top"), -3, 20, 13, 90, 0, 0, 1, true, *EF_FLIP_YZ);
-        macros::LAST_PARTICLE_SET_COLOR(agent, 0.104, 0.024, 0.754);
-    }
-    frame(agent.lua_state_agent, 18.0);
-    if macros::is_excute(agent) {
-        macros::EFFECT(agent, Hash40::new("sys_crown"), Hash40::new("top"), 12, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
-    }
-    frame(agent.lua_state_agent, 37.0);
-    if macros::is_excute(agent) {
-        macros::EFFECT_FOLLOW_FLIP(agent, Hash40::new("sys_attack_speedline"), Hash40::new("sys_attack_speedline"), Hash40::new("top"), -3, 20, 13, 90, 0, 0, 1, true, *EF_FLIP_YZ);
-        macros::LAST_PARTICLE_SET_COLOR(agent, 0.104, 0.024, 0.754);
-    }
-    frame(agent.lua_state_agent, 38.0);
-    if macros::is_excute(agent) {
-        macros::EFFECT(agent, Hash40::new("sys_crown"), Hash40::new("top"), 12, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, false);
-    }
-}
-
-// DOWN THROW SOUND
-unsafe extern "C" fn ridley_sound_throwlw(agent: &mut L2CAgentBase) {
-    frame(agent.lua_state_agent, 2.0);
-    if macros::is_excute(agent) {
-        macros::PLAY_SE(agent, Hash40::new("se_common_throw_01"));
-    }
-    wait(agent.lua_state_agent, 15.0);
-    if macros::is_excute(agent) {
-        macros::PLAY_SE(agent, Hash40::new("se_common_throw_03"));
-    }
-    frame(agent.lua_state_agent, 18.0);
-    if macros::is_excute(agent) {
-        macros::PLAY_SE(agent, Hash40::new("se_common_down_l_01"));
-        macros::PLAY_SE(agent, Hash40::new("se_common_kick_hit_m"));
-    }
-    frame(agent.lua_state_agent, 38.0);
-    if macros::is_excute(agent) {
-        macros::PLAY_SE(agent, Hash40::new("se_common_down_l_01"));
-        macros::PLAY_SE(agent, Hash40::new("se_common_kick_hit_m"));
-    }
-}
-
-// DOWN THROW EXPRESSION
-unsafe extern "C" fn ridley_expression_throwlw(agent: &mut L2CAgentBase) {
-    if macros::is_excute(agent) {
-        slope!(agent, *MA_MSC_CMD_SLOPE_SLOPE, *SLOPE_STATUS_LR);
-    }
-    frame(agent.lua_state_agent, 18.0);
-    if macros::is_excute(agent) {
-        ControlModule::set_rumble(agent.module_accessor, Hash40::new("rbkind_attackm"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
-    }
-    frame(agent.lua_state_agent, 21.0);
-    if macros::is_excute(agent) {
-        macros::QUAKE(agent, *CAMERA_QUAKE_KIND_L);
-    }
-    frame(agent.lua_state_agent, 38.0);
-    if macros::is_excute(agent) {
-        ControlModule::set_rumble(agent.module_accessor, Hash40::new("rbkind_attackm"), 0, false, *BATTLE_OBJECT_ID_INVALID as u32);
-    }
-    frame(agent.lua_state_agent, 41.0);
-    if macros::is_excute(agent) {
-        macros::QUAKE(agent, *CAMERA_QUAKE_KIND_L);
-    }
-}
-
-*/
 pub fn install() {
     Agent::new("ridley")
         .game_acmd("game_catch", ridley_catch, Low)
@@ -183,12 +76,6 @@ pub fn install() {
         .game_acmd("game_catchdash", ridley_catchdash, Low)
 
         .game_acmd("game_catchturn", ridley_catchturn, Low)
-        /*
-        .game_acmd("game_throwlw", ridley_throwlw, Low)
-        .effect_acmd("effect_throwlw", ridley_effect_throwlw, Low)
-        .sound_acmd("sound_throwlw", ridley_sound_throwlw, Low)
-        .expression_acmd("expression_throwlw", ridley_expression_throwlw, Low)
-        */
 
         .install();
 }
